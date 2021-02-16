920cce1c26db9b947e936438f23ba2d2551d3a37
henremurr <77940622+henremurr@users.noreply.github.com>
2021-02-16 11:51:28 -0700

    asdasd

--- provider.tf
+++
@@ -1,5 +0,0 @@
-resource "oci_identity_compartment" "Test_Compartments" {
-        compartment_id = "${var.root_compartment_ocid}"
-        description = "POCCloudAdminmanagedcompartmentforvariousBU"
-        name = "POC_Compartment"
-}
